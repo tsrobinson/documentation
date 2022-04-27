677bdd68be149f9e2e833cb39e6dc000533a671f
Peter Heywood <peethwd@gmail.com>
2022-04-27 16:45:48 +0100

    Disable the annotation, but keep the javascript link processing for future re-use

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -3,15 +3,15 @@ sphinx-book-theme announcements are pure html,
 so cannot use sphinx :ref: for output destination URIs relative to the current page. 
 Instead, find the appropriate link within the current page, to add the anchor to the announcement.
 */
-window.onload = function () {
-    expectedAnnouncementContent = "RHEL 8 Migration";
-    expectedAnchorContent = "RHEL 8 Migration";
+/* window.onload = function () {
+    expectedAnnouncementContent = "";
+    expectedAnchorContent = "";
     var elements = document.getElementsByClassName("announcement");
     for (var i = 0; i < elements.length; i++) {
         var element = elements.item(i);
         originalContent = element.innerHTML;
         if(originalContent.toLowerCase().includes(expectedAnnouncementContent.toLowerCase())) {
-            // Grab the link to the RHEL 8 migration page from the toc.
+            // Grab the link to the destination page from the toc.
             anchorList = document.querySelectorAll(".toctree-l1>a.reference.internal");
             for (var j = 0; j < anchorList.length; j++) {
                 anchor = anchorList[j];
@@ -22,4 +22,4 @@ window.onload = function () {
             }
         }
     }
-}
+} */
